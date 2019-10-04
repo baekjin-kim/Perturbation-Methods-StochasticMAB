e6c8547981f405398347412f1575f494582107cc
Baekjin Kim <skybackj@gmail.com>
2019-10-04 14:49:29 -0400

    Create algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,319 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+%matplotlib inline
+import os
+
+##########################################################################################
+#UCB1 algorithm
+##########################################################################################
+class ucb_bandit:
+    def __init__(self, k, c, iters, mu='random'):
+        # Number of arms
+        self.k = k
+        # Exploration parameter
+        self.c = c
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 1
+        # Step count for each arm
+        self.k_n = np.ones(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.uniform(-1, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        
+    def pull(self):
+        # Select action according to UCB Criteria
+        a = np.argmax(self.k_reward + self.c * np.sqrt((np.log(self.n)) / self.k_n))    
+        reward = reward_generate(self.mu[a])
+        
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = np.max(self.mu)-self.mean_reward
+            
+    def reset(self, mu=None):
+        # Resets results while keeping settings
+        self.n = 1
+        self.k_n = np.ones(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(self.k)
+        if mu == 'random':
+            self.mu = np.random.uniform(-1, 1, k)
+   
+##########################################################################################   
+#FTPL algorithm via Gaussian perturbation
+##########################################################################################
+class RCB_gaussian:
+    def __init__(self, k, c, iters, mu='random'):
+        # Number of arms
+        self.k = k
+        # Exploration parameter
+        self.c = c
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 1
+        # Step count for each arm
+        self.k_n = np.ones(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.uniform(-1, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        
+    def pull(self):
+        # Select action
+        a = np.argmax(self.k_reward + self.c * np.random.normal(0, 1, self.k) / np.sqrt(self.k_n))
+        reward = reward_generate(self.mu[a])
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = np.max(self.mu)-self.mean_reward
+            
+    def reset(self, mu=None):
+        # Resets results while keeping settings
+        self.n = 1
+        self.k_n = np.ones(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(self.k)
+        if mu == 'random':
+            self.mu = np.random.uniform(-1, 1, k)
+
+##########################################################################################
+#FTPL algorithm via Double Exponential perturbation  
+##########################################################################################
+  class RCB_DE:
+    def __init__(self, k, c, iters, mu='random'):
+        # Number of arms
+        self.k = k
+        # Exploration parameter
+        self.c = c
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 1
+        # Step count for each arm
+        self.k_n = np.ones(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.uniform(-1, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        
+    def pull(self):
+        # Select action according to UCB Criteria
+        a = np.argmax(self.k_reward + self.c * np.random.laplace(0, 1, self.k) / np.sqrt(self.k_n))
+        reward = reward_generate(self.mu[a])
+        
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = np.max(self.mu)-self.mean_reward
+            
+    def reset(self, mu=None):
+        # Resets results while keeping settings
+        self.n = 1
+        self.k_n = np.ones(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(self.k)
+        if mu == 'random':
+            self.mu = np.random.uniform(-1, 1, k)
+ 
+##########################################################################################
+#FTPL algorithm via Uniform perturbation   
+##########################################################################################
+class RCB_uniform:
+    def __init__(self, k, c, iters, mu='random'):
+        # Number of arms
+        self.k = k
+        # Exploration parameter
+        self.c = c
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 1
+        # Step count for each arm
+        self.k_n = np.ones(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.uniform(-1, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        
+    def pull(self):
+        # Select action according to UCB Criteria
+        a = np.argmax(self.k_reward + np.random.uniform(-1, 1, k) * self.c * np.sqrt(np.log(self.n) / self.k_n))
+        reward = reward_generate(self.mu[a])
+        
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = np.max(self.mu)-self.mean_reward
+            
+    def reset(self, mu=None):
+        # Resets results while keeping settings
+        self.n = 1
+        self.k_n = np.ones(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(self.k)
+        if mu == 'random':
+            self.mu = np.random.uniform(-1, 1, k)
+
+##########################################################################################
+#FTPL algorithm via Rademacher perturbation     
+##########################################################################################
+  class RCB_rademacher:
+    def __init__(self, k, c, iters, mu='random'):
+        # Number of arms
+        self.k = k
+        # Exploration parameter
+        self.c = c
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 1
+        # Step count for each arm
+        self.k_n = np.ones(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.uniform(-1, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        
+    def pull(self):
+        # Select action according to UCB Criteria
+        a = np.argmax(self.k_reward + (2* np.random.binomial(1,0.5,k)-1) * self.c * np.sqrt(np.log(self.n) / self.k_n))
+        reward = reward_generate(self.mu[a])
+        
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = np.max(self.mu)-self.mean_reward
+            
+    def reset(self, mu=None):
+        # Resets results while keeping settings
+        self.n = 1
+        self.k_n = np.ones(self.k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(self.k)
+        if mu == 'random':
+            self.mu = np.random.uniform(-1, 1, k)
